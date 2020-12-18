469ca4a2c0dfd3a5f6bc569505ea8e6c34d8c3b6
Faisal <faisalyusuf98@gmail.com>
2020-12-19 01:53:29 +0500

    Added the header/nav bar, in order to add components to the main html file, it is required to bootstrap them in the app ts file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopheaderComponent } from './topheader/topheader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopheaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ],
+  bootstrap: [AppComponent,TopheaderComponent]
 })
 export class AppModule { }
---
+++ src/app/topheader/topheader.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topheader',
+  templateUrl: './topheader.component.html',
+  styleUrls: ['./topheader.component.css']
+})
+export class TopheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    console.log("This works");
+   }
+
+
+
+
+}
