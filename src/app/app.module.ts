a6dfe40e425e673bdaf671e7cbfaa40808b46e42
arkilo <faisalyusuf98@gmail.com>
2020-12-25 03:12:02 +0500

    Downgraded angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopheaderComponent } from './topheader/topheader.component';
-import { TestcommandsComponent } from './testcommands/testcommands.component';
-import { InfopanelverticalComponent } from './infopanelvertical/infopanelvertical.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopheaderComponent,
-    TestcommandsComponent,
-    InfopanelverticalComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [ ],
-  bootstrap: [AppComponent,TopheaderComponent,TestcommandsComponent]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
