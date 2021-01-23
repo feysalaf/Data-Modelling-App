cf7f2b9f9626ba60883bc8676e0323be82eccce6
arkilo <faisalyusuf98@gmail.com>
2021-01-23 16:17:31 +0500

    Created the binding of fe button to typescript. The observer object watches the subject and whenever a change is observed, it executes a method. In order to execute a method outside current method, do this.<method>(). Where <method> is some method outside the scope of current

--- src/app/verticalsidebar/verticalsidebar.component.ts
+++ src/app/verticalsidebar/verticalsidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { fade, stretchout  } from '../../assets/animations/animations';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-verticalsidebar',
@@ -8,10 +9,32 @@ import { fade, stretchout  } from '../../assets/animations/animations';
   animations:[fade,stretchout ]
 })
 export class VerticalsidebarComponent implements OnInit {
+  constructor() {
 
-  constructor() { }
+  }
+
+  mymethod(){
+    let z:number;
+    let x = 1;
+    let y = 2;
+    z = x+y;
+    return z;
+  }
+
+  ngAfterViewInit() {
+    let button = document.querySelector('.buttonfont');
+    let buttonobserver = fromEvent(button, 'click');
+    //whenever the button is pressed, the buttonobserver knows it
+    //and then it executes a function upon the CLICK EVENT
+    let method_returned:number;
+    method_returned = this.mymethod();
+
+    const uponeventexecute = buttonobserver.subscribe(() => {console.log(method_returned);})
+    }
+
+
 
   ngOnInit(): void {
-  }
+   }
 
 }
