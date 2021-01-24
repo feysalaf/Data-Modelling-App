ef7643b21ac47f987128fc148189f56b0946428e
arkilo <faisalyusuf98@gmail.com>
2021-01-24 14:49:22 +0500

    Placed server imports in a separate file, added an index file to show when they server is running, and created a general structure for the lib files for the server

---
+++ src/server/lib/imports/server_imports.py
@@ -0,0 +1,16 @@
+#                   SERVER DEPENDENCIES            #
+
+from flask import Flask, jsonify, render_template, request
+from flask_socketio import SocketIO, send
+import math
+import os
+import time
+#module imports
+from random import randrange
+import json
+import webview
+from subprocess import call
+from threading import Timer
+import subprocess
+import threading
+import sys
--- src/server/server.py
+++ src/server/server.py
@@ -1,26 +1,17 @@
-from flask import Flask, jsonify, render_template, request
-from flask_socketio import SocketIO, send
-import math
-import os
-import time
-#module imports
-from random import randrange
-import json
-import webview
-from subprocess import call
-from threading import Timer
-import subprocess
-import threading
-import sys
+from lib.imports.server_imports import *
 #importing user libraries
-from Lib.Exponential.module import *
+from lib.exponential.module import *
+from lib.RandomGeneration import *
 
-
+#base initialization
 app = Flask(__name__,template_folder ='templates/')
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 #instantiating the socket io object
 socketio = SocketIO(app)
 
+
+
+######################################################
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
@@ -61,47 +52,14 @@ def send_bulk_json_out():
     socketio.emit('send_to_js',(returned_json))
 
 
-###################################################################################
+################################################### 
 
-
-
-#@app.route("/api/calc",methods=['GET','POST'])
-# def add():
-#     a = int(request.args.get('a', 0))
-#     b = int(request.args.get('b', 0))
-#     div = 'na'
-#     if b != 0:
-#         div = a/b
-#     return jsonify({
-#         "a"        :  a,
-#         "b"        :  b,
-#         "add"      :  a+b,
-
-#     })
 
 @app.route("/display/",methods=['GET','POST'])
-def iterator():
-     for i in range(1000):
-        random_var = randrange(1000)
-        time.sleep(4)
+#call iterator
 
-        return jsonify({
-            "add" : random_var,
-        })
 
-    #return render_template("index.html",value_4 = my_strings)
-
-def random_gen():
-    for i in range(1000):
-            random_var_1 = randrange(1000)
-            random_var_2 = randrange(100)
-            random_var_3 = randrange(10)
-
-            time.sleep(2)
-            out = {'output_1' : str(random_var_1),'output_2' : str(random_var_2),'output_3': str(random_var_3)}
-            print(out)
-            socketio.emit('module_trigger_stream',(out))
-
+#random_gen method just sitting without decorator
 
 @app.route("/subprocess",methods=['GET','POST'])
 def add():
@@ -124,16 +82,8 @@ def render_output():
     return render_template("index.html", value_1=my_string, value_2 = val_2,value_3 = val_3 )
 
 
-
-# def call_frameless_window():
-#     call(["python3", "frameless_window.py"])
-
-# def call_frameless_window_via_terminal():
-#     subprocess.run(["python3" ,"frameless_window.py"])
 def start_server():
     socketio.run(app,debug=True)
-
-
 
 
 
