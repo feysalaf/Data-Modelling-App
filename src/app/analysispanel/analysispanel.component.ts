311401f5e25ee73588057bc3fc9fb3321adf541b
arkilo <faisalyusuf98@gmail.com>
2021-02-03 00:37:05 +0500

    Experimenting with svg, everything might go to sh*t

--- src/app/analysispanel/analysispanel.component.ts
+++ src/app/analysispanel/analysispanel.component.ts
@@ -1,13 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { fade, stretchout  } from '../../assets/animations/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-analysispanel',
   templateUrl: './analysispanel.component.html',
-  styleUrls: ['./analysispanel.component.css']
+  styleUrls: ['./analysispanel.component.css'],
+  animations:[fade,stretchout,
+  trigger('special',[
+
+      state('void',style({"transform":"scaleX(0.2)"})),
+      state('*',style({"transform":"scaleX(1)"})),
+      // transition('void <=> *',[
+      // style({opacity:0}),
+      // animate(2200)]),
+
+      transition('void => *',[
+      style({ }),
+      animate(1800)])])
+ ]
+
 })
 export class AnalysispanelComponent implements OnInit {
+  // width:number;
+  controlvar:string;
+   @Input() width:number;
 
   constructor() { }
+
+  ngAfterViewInit() {
+
+
+    setTimeout(()=>{this.width=187;this.controlvar='yes'},2000);
+    setTimeout(()=>{this.width=287;this.controlvar='yes'},6000);
+
+  }
 
   ngOnInit(): void {
   }
