ea6474170c4799be4f16796bc05f5e21ba9b9a57
arkilo <faisalyusuf98@gmail.com>
2021-02-11 18:08:36 +0500

    Generalized the function

--- src/server/lib/seaborn/graph.py
+++ src/server/lib/seaborn/graph.py
@@ -18,23 +18,35 @@ sb.set_theme(style="whitegrid")
 #                 n
 #         ]
 #
-# TODO: generalize
-def generate_data():
+# TODO: work for negative values
+def generate_data(function:str,ranges:dict):
     #declare data container(list)
     datalist = []
     # print(datalist[0])
-    for i in range(0,10):
-        print(i)
+    for i in range(ranges['start'],ranges['end']):
         #index new data to
         #to ith element in
         #the list + add dict to that entry
-        datalist.append({"x":i,"y":2*i**2})
+        datalist.append({"x":i,"y":function(i)})
     print(datalist)
 
 
 
-generate_data()
-sb.set(rc={"figure.figsize": (8, 4)}); np.random.seed(0)
-x = np.random.randn(100)
-ax = sb.displot(x)
-plt.show()
+ranges = {"start":0,"end":10}
+
+#function
+def LinearFunction(i):
+    return 2 * i ** 2
+
+def ExponentialFunction(i):
+    return 32 * i
+
+generate_data(LinearFunction,ranges)
+generate_data(ExponentialFunction,ranges)
+
+
+
+# sb.set(rc={"figure.figsize": (8, 4)}); np.random.seed(0)
+# x = np.random.randn(100)
+# ax = sb.displot(x)
+# plt.show()
